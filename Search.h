06587942e6755b9cc065f5e436fe718e6d838149
Chijioke Kamanu <jioke.k@gmail.com>
2018-11-24 17:01:06 -0500

    Create Search.h

---
+++ Search.h
@@ -0,0 +1,10 @@
+#ifndef SEARCH_H
+#define SEARCH_H
+#include <vector>
+
+template <class dataType> class Search {
+  public:
+    static dataType LinearSearch(vector<dataType>, dataType); //Function Prototype for Linear Search
+};
+
+#endif
