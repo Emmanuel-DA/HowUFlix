38600a5539305314a567ea8df8181e3c367535c2
Chijioke Kamanu <jioke.k@gmail.com>
2018-11-24 17:00:12 -0500

    Create Sort.h

---
+++ Sort.h
@@ -0,0 +1,9 @@
+#ifndef SORT_H
+#define SORT_H
+#include <vector>
+
+template <class dataType> class Sort {
+  public:
+    static void InsertionSort(vector<dataType>); //Function Prototype for Merge Sort
+};
+#endif
